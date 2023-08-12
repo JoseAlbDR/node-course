9f24b00e74e3414a0771496881906b817ccd203f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 11:39:27 +0200

    feat: request event HTTP

--- 14-request-event.js
+++ 14-request-event.js
@@ -1,11 +1,11 @@
-import { createServer } from "http";
+const http = require("http");
 
 // const server = http.createServer((req, res) => {
 //   res.end('Welcome')
 // })
 
 // Using Event Emitter API
-const server = createServer();
+const server = http.createServer();
 // emits request event
 // subcribe to it / listen for it / respond to it
 server.on("request", (req, res) => {
