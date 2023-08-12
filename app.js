a312b4130ab67e8d53e2590a80152c6a8130e179
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 10:56:18 +0200

    feat: clearInterval y setInterval example

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-import _ from "lodash";
+let i = 0;
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const interval = setInterval(() => {
+  console.log("hello world");
+  i++;
+  console.log(i);
+  if (i === 5) clearInterval(interval);
+}, 2000);
+
+console.log(`I will run first`);
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
