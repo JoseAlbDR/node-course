3a4c2c7ead9412909c72b0052ac8a9513505c3a8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-12 11:52:03 +0200

    feat: http stream

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+import { createServer } from "http";
 import { createReadStream } from "fs";
 
-// default 64kb
-// last buffer - remainder
-// highWaterMark - control size
-// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
-// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream("./content/big.txt", "utf8");
-
-stream.on("data", (result) => {
-  console.log(result);
-});
-stream.on("error", (err) => console.log(err));
+createServer(function (req, res) {
+  // const text = fs.readFileSync('./content/big.txt', 'utf8')
+  // res.end(text)
+  const fileStream = createReadStream("./content/big.txt", "utf8");
+  fileStream.on("open", () => {
+    fileStream.pipe(res);
+  });
+  fileStream.on("error", (err) => {
+    res.end(err);
+  });
+}).listen(5000);
